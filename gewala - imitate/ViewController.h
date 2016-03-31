40d48cd8c22a3e62209805992bab9cee30943fd2
Yesi <317447721@qq.com>
2016-03-31 14:23:33 +0800

    Initial Commit

---
+++ gewala - imitate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gewala - imitate
+//
+//  Created by Yesi on 16/3/31.
+//  Copyright © 2016年 Yesi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gewala - imitateTests/gewala___imitateTests.m
@@ -0,0 +1,39 @@
+//
+//  gewala___imitateTests.m
+//  gewala - imitateTests
+//
+//  Created by Yesi on 16/3/31.
+//  Copyright © 2016年 Yesi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface gewala___imitateTests : XCTestCase
+
+@end
+
+@implementation gewala___imitateTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
